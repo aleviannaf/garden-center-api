ad2ac19906b5b44bc25e6f99f77a124cd84b294c
aleviannaf <alexandrevianna511@gmail.com>
2023-10-30 12:08:08 -0400

    refactor: using typeorm in service

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,13 +1,11 @@
-import { QueryResult } from "pg"
-import { client } from "../database"
-import { IProduct, IProductList } from "../interface"
+import { AppDataSource } from "../data-source";
+import Product from "../entities/Product.entity";
+import { Repository } from "typeorm";
 
-const listProduct = async (): Promise<IProductList> => {
-    const queryString: string = `SELECT * FROM products;`
-
-    const queryResult: QueryResult<IProduct> = await client.query(queryString)
-
-    return queryResult.rows
+const listProduct = async (): Promise<Product[]> => {
+    const repo: Repository<Product> = AppDataSource.getRepository(Product);
+    const products: Product[] = await repo.find()
+    return products
 }
 
 export default { listProduct}
