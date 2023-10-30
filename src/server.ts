de2116770ab05520e36d5af1af532855eb56eac9
aleviannaf <alexandrevianna511@gmail.com>
2023-10-30 12:03:53 -0400

    config: configured typeorm on application

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
-import app from "./app";
-import { startDatabase } from "./database";
+import 'dotenv/config'
+import app from "./app"
+import { AppDataSource } from './data-source'
 
-const PORT: number = Number(process.env.PORT || 3000);
+AppDataSource.initialize()
+    .then((): void => {
+        console.log('Database connected')
 
-app.listen(PORT, async () => {
-  await startDatabase();
-  console.log(`App running on port ${PORT}`);
-});
+        const PORT: number = Number(process.env.PORT) | 3000
+
+        app.listen(PORT, (): void => console.log(`App is running at port ${PORT}`))
+    })
+    .catch((error: unknown) => console.log(error))
