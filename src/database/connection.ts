4b8f0ba96dc7b7cb1547237765cb5c3615cad451
aleviannaf <alexandrevianna511@gmail.com>
2023-10-30 12:04:56 -0400

    refactor: removed pg

--- src/database/connection.ts
+++
@@ -1,8 +0,0 @@
-import client from "./config";
-
-const startDatabase = async (): Promise<void> => {
-  await client.connect()
-  console.log("Database started.")
-};
-
-export default startDatabase;
