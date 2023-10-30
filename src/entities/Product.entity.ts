a46616f17f427c1f65ff9b9604e9321fcb8c4c87
aleviannaf <alexandrevianna511@gmail.com>
2023-10-30 12:06:46 -0400

    feat: created entity product

---
+++ src/entities/Product.entity.ts
@@ -0,0 +1,22 @@
+import {Entity, Column, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity("products")
+export default class Product {
+    @PrimaryGeneratedColumn('increment')
+    id: number
+
+    @Column({length: 90, unique: true})
+    name: string
+
+    @Column({length:255})
+    description: string
+
+    @Column({length: 45})
+    type: string
+
+    @Column({default: 0})
+    price: number  
+
+    @Column({length: 255})
+    image_url: string
+}
