43b15a3a40f2ee2f3339c8d1ccde4deaa901c89d
aleviannaf <alexandrevianna511@gmail.com>
2023-12-11 20:28:08 -0400

    style: removed console.log

--- src/services/shippingAddress.service.ts
+++ src/services/shippingAddress.service.ts
@@ -6,7 +6,6 @@ const verifyShippingAddress = async (cep: number): Promise<ICepData> => {
     const response = await axios.get<ICepData>("https://viacep.com.br/ws/"+cep+"/json/")
 
     if ('erro' in response.data && response.data.erro === true) {
-        console.log('Erro encontrado na resposta.');
         throw new AppError('Address not found', 404)
     } else {
         return response.data
